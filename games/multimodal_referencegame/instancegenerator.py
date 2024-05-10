51e74bfda666261ae2f7679f175ee91d5d81d843
kushal-10 <kushalkoshti11@gmail.com>
2024-05-10 09:53:32 +0200

    update multimodal ref game

--- games/multimodal_referencegame/instancegenerator.py
+++ games/multimodal_referencegame/instancegenerator.py
@@ -10,6 +10,8 @@ import random
 import clemgame
 from clemgame.clemgame import GameInstanceGenerator
 import shutil
+import matplotlib.pyplot as plt
+import json
 
 random.seed(123)
 
@@ -50,123 +52,94 @@ class ReferenceGameInstanceGenerator(GameInstanceGenerator):
         random_index = random.randint(0, len(images)-1)
         return images[random_index]
 
+    def plot_grid(self, grid, file_path):
+        fig, ax = plt.subplots()
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+        for y in range(len(grid)):
+            for x in range(len(grid[y])):
+                if grid[y][x] == "▢":
+                    ax.add_patch(plt.Rectangle((x, -y - 1), 1, 1, facecolor="white", edgecolor="black"))
+                else:
+                    ax.add_patch(plt.Rectangle((x, -y - 1), 1, 1, facecolor="white", edgecolor="black"))
+                    ax.text(x + 0.5, -y - 0.5, grid[y][x], ha='center', va='center', color='black', fontsize=12)
+
+        ax.autoscale_view()
+        # plt.show()
+        plt.savefig(file_path)
+
+    def process_grid(self, saved_grids, grid):
+
+        if grid not in saved_grids:
+            ascii_grid = []
+            lines = grid.split('\n')
+            for l in lines:
+                line = l.split(' ')
+                ascii_grid.append(line)
+
+            file_path = f"resources/grid_images/{len(saved_grids)}.png"
+            self.plot_grid(ascii_grid, file_path)
+
+            saved_grids[grid] = 'games/multimodal_referencegame/'+file_path
+        return saved_grids[grid]
+
     def generate_grid_instances(self):
         # GRID EXPERIMENT
         player_a_prompt_header = self.load_template(f"resources/initial_prompts/player_a_prompt_images.template")
         player_b_prompt_header = self.load_template(f"resources/initial_prompts/player_b_prompt_images.template")
 
-        grid_dataset = self.load_json('resources/grid_files.json')
+        instances = {}
+        saved_grids = {}
 
-        game_counter = 0
-        experiment = self.add_experiment('grids')
-        while True:
+        with open('resources/ascii_game_instances.json') as json_file:
+            instances = json.load(json_file)
 
-            if game_counter == 30:
-                break
+        for exp in instances['experiments']:
 
-            for target_category in grid_dataset:
+            game_counter = 0
+            experiment = self.add_experiment(exp['name'])
 
-                target_category_images = grid_dataset[target_category]
-                target_image = self.select_random_item(target_category_images)
-                target_image_path = f"games/multimodal_referencegame/resources/{target_image}"
 
-                # remove the target image from the list, select another image from the same category
-                target_category_images.remove(target_image)
-                distractor1 = self.select_random_item(target_category_images)
-                distractor1_path = f"games/multimodal_referencegame/resources/{distractor1}"
 
-                # get all image categories, remove the target category
-                all_categories = list(grid_dataset.keys())
-                all_categories.remove(target_category)
-                distractor2_category = self.select_random_item(all_categories)
-                distractor2 = self.select_random_item(grid_dataset[distractor2_category])
-                distractor2_path = f"games/multimodal_referencegame/resources/{distractor2}"
+            for instance in exp['game_instances']:
 
-                for i in [1, 2, 3]:
+                player1_target_grid =self.process_grid(saved_grids, instance['player_1_target_grid'])
 
-                    player_a_prompt_header = self.load_template(
-                        f"resources/initial_prompts/player_a_prompt_images.template")
-                    game_instance = self.add_game_instance(experiment, game_counter)
+                player_1_second_grid = self.process_grid(saved_grids, instance['player_1_second_grid'])
 
-                    player_1_first_image = ""
-                    player_1_second_image = ""
-                    player_1_third_image = ""
-                    player_2_first_image = ""
-                    player_2_second_image = ""
-                    player_2_third_image = ""
+                player_1_third_grid = self.process_grid(saved_grids, instance['player_1_third_grid'])
 
-                    if i == 1:
-                        player_1_first_image = target_image_path
-                        player_1_second_image = distractor1_path
-                        player_1_third_image = distractor2_path
+                player_2_first_grid = self.process_grid(saved_grids, instance['player_2_first_grid'])
+                player_2_second_grid = self.process_grid(saved_grids, instance['player_2_second_grid'])
+                player_2_third_grid = self.process_grid(saved_grids, instance['player_2_third_grid'])
 
-                        player_2_first_image = distractor1_path
-                        player_2_second_image = target_image_path
-                        player_2_third_image = distractor2_path
+                game_instance = self.add_game_instance(experiment, game_counter)
 
-                        player_2_target_name = "second"
+                game_instance["player_1_prompt_header"] = player_a_prompt_header.replace('FIRST_IMAGE', 'the target').replace('SECOND_IMAGE', 'a distractor').replace('THIRD_IMAGE', 'a distractor')
+                game_instance['player_1_first_image'] = player1_target_grid
+                game_instance['player_1_second_image'] = player_1_second_grid
+                game_instance['player_1_third_image'] = player_1_third_grid
 
-                        player_a_prompt_header = player_a_prompt_header.replace("FIRST_IMAGE", "the target").replace(
-                            "SECOND_IMAGE", "a distractor").replace("THIRD_IMAGE", "a distractor")
+                game_instance["player_2_prompt_header"] = player_b_prompt_header
+                game_instance['player_2_first_image'] = player_2_first_grid
+                game_instance['player_2_second_image'] = player_2_second_grid
+                game_instance['player_2_third_image'] = player_2_third_grid
+                game_instance['target_image_name'] = instance['target_grid_name']
+                game_instance['player_1_response_pattern'] = '^expression:\s(?P<content>.+)\n*(?P<remainder>.*)'
+                # named groups:
+                # 'content' captures only the generated referring expression
+                # 'remainder' should be empty (if models followed the instructions)
+                game_instance[
+                    'player_2_response_pattern'] = '^answer:\s(?P<content>first|second|third)\n*(?P<remainder>.*)'
+                # 'content' can directly be compared to gold answer
+                # 'remainder' should be empty (if models followed the instructions)
 
-                    elif i == 2:
-                        player_1_first_image = distractor2_path
-                        player_1_second_image = distractor1_path
-                        player_1_third_image = target_image_path
+                # the following two fields are no longer required, but kept for backwards compatibility with previous instance versions
+                game_instance["player_1_response_tag"] = "expression:"
+                game_instance["player_2_response_tag"] = "answer:"
 
-                        player_2_first_image = target_image_path
-                        player_2_second_image = distractor1_path
-                        player_2_third_image = distractor2_path
-
-                        player_2_target_name = "first"
-
-                        player_a_prompt_header = player_a_prompt_header.replace("FIRST_IMAGE", "a distractor").replace(
-                            "SECOND_IMAGE", "a distractor").replace("THIRD_IMAGE", "the target")
-
-                    elif i == 3:
-                        player_1_first_image = distractor2_path
-                        player_1_second_image = target_image_path
-                        player_1_third_image = distractor1_path
-
-                        player_2_first_image = distractor2_path
-                        player_2_second_image = distractor1_path
-                        player_2_third_image = target_image_path
-
-                        player_2_target_name = "third"
-
-                        player_a_prompt_header = player_a_prompt_header.replace("FIRST_IMAGE", "a distractor").replace(
-                            "SECOND_IMAGE", "the target").replace("THIRD_IMAGE", "a distractor")
-
-                    game_instance["player_1_prompt_header"] = player_a_prompt_header
-                    game_instance['player_1_first_image'] = player_1_first_image
-                    game_instance['player_1_second_image'] = player_1_second_image
-                    game_instance['player_1_third_image'] = player_1_third_image
-
-                    game_instance["player_2_prompt_header"] = player_b_prompt_header
-                    game_instance['player_2_first_image'] = player_2_first_image
-                    game_instance['player_2_second_image'] = player_2_second_image
-                    game_instance['player_2_third_image'] = player_2_third_image
-                    game_instance['target_image_name'] = player_2_target_name
-                    game_instance['player_1_response_pattern'] = '^expression:\s(?P<content>.+)\n*(?P<remainder>.*)'
-                    # named groups:
-                    # 'content' captures only the generated referring expression
-                    # 'remainder' should be empty (if models followed the instructions)
-                    game_instance[
-                        'player_2_response_pattern'] = '^answer:\s(?P<content>first|second|third)\n*(?P<remainder>.*)'
-                    # 'content' can directly be compared to gold answer
-                    # 'remainder' should be empty (if models followed the instructions)
-
-                    # the following two fields are no longer required, but kept for backwards compatibility with previous instance versions
-                    game_instance["player_1_response_tag"] = "expression:"
-                    game_instance["player_2_response_tag"] = "answer:"
-
-                    game_counter += 1
-
-                    if game_counter >= MAX_NUMBER_INSTANCES:
-                        break
-
-                if game_counter >= MAX_NUMBER_INSTANCES:
-                    break
+                game_counter += 1
 
     def generate_scene_instances(self):
         player_a_prompt_header = self.load_template(f"resources/initial_prompts/player_a_prompt_images.template")
@@ -176,7 +149,7 @@ class ReferenceGameInstanceGenerator(GameInstanceGenerator):
 
         game_counter = 0
         image_counter = 1
-        experiment = self.add_experiment('scenes')
+        experiment = self.add_experiment('AED_scenes')
         for target_category in aed_dataset:
 
             target_category_images = aed_dataset[target_category]
