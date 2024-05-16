e6de59663449df91eedc45414247b667217f3033
kushal-10 <kushalkoshti11@gmail.com>
2024-05-16 18:02:28 +0200

    rm prints

--- backends/huggingface_multimodal_api.py
+++ backends/huggingface_multimodal_api.py
@@ -343,7 +343,5 @@ class HuggingfaceMultimodalModel(backends.Model):
             rt_split = response_text.split(self.cull) # Cull from End of String token
             response_text = rt_split[0]
         response_text = response_text.strip()
-        
-        print(f"RESPONSE: {response_text}")
 
         return prompt, response, response_text
