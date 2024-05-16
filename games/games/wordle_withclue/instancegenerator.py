45910acce64bcd7b11a0f87abf8f8633ca58e8d9
kushal-10 <kushalkoshti11@gmail.com>
2024-05-16 19:00:16 +0200

    rm extra games folder

--- games/games/wordle_withclue/instancegenerator.py
+++
@@ -1,8 +0,0 @@
-import random
-
-from games.wordle.instancegenerator import WordleGameInstanceGenerator
-
-GAME_NAME = "wordle_withclue"
-
-if __name__ == "__main__":
-    WordleGameInstanceGenerator(GAME_NAME).generate()
