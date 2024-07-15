0eaa42553317916278c7ba5ebd6bcff66c35b473
kushal-10 <kushalkoshti11@gmail.com>
2024-07-15 13:04:26 +0200

    hardcode tokenizer in model

--- backends/huggingface_multimodal_api.py
+++ backends/huggingface_multimodal_api.py
@@ -161,6 +161,7 @@ class HuggingfaceMultimodalModel(backends.Model):
         self.model_name = model_spec['model_name']
         self.processor = load_processor(model_spec)
         self.multimodal_model = load_model(model_spec)
+        self.multimodal_model.tokenizer = self.processor # Hardcode for internLM
         self.split_prefix = model_spec['output_split_prefix']
         self.context_size = get_context_limit(model_spec)
 
