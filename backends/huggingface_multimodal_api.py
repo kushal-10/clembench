1d08b16df3f0e8d7dd5fdb9a0ef538e465eba69e
kushal-10 <kushalkoshti11@gmail.com>
2024-07-16 12:53:56 +0200

    check additons

--- backends/huggingface_multimodal_api.py
+++ backends/huggingface_multimodal_api.py
@@ -203,6 +203,7 @@ class HuggingfaceMultimodalModel(backends.Model):
         kwargs = {"template": self.template}
         outs = model_response.prepare_inputs(messages=messages, **kwargs)
         prompt_text, image, additions = outs['prompt'], outs['image'], outs['kwargs']
+        print(f"Print additions : {additions}")
 
         prompt_tokens = model_response.get_tokens(prompt=prompt_text, processor=self.processor, **additions)
 
