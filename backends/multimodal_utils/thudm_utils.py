b6368c17b90d210c3b85f7c03c0902e621b50859
kushal-10 <kushalkoshti11@gmail.com>
2024-09-01 15:08:43 +0200

    test with image as a list

--- backends/multimodal_utils/thudm_utils.py
+++ backends/multimodal_utils/thudm_utils.py
@@ -18,60 +18,59 @@ model = AutoModelForCausalLM.from_pretrained(
 
 text_only_template = "A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: {} ASSISTANT:"
 
-while True:
-    image_path = "games/multimodal_referencegame/resources/clevr_images/1.jpg"
-    if image_path == '':
-        print('You did not enter image path, the following will be a plain text conversation.')
-        image = None
-        text_only_first_query = True
+image_path = "games/multimodal_referencegame/resources/clevr_images/1.jpg"
+if image_path == '':
+    print('You did not enter image path, the following will be a plain text conversation.')
+    image = None
+    text_only_first_query = True
+else:
+    image = Image.open(image_path).convert('RGB')
+
+history = []
+image_list = [image]
+
+query = "What is shown in this image"
+
+if image_list is None:
+    if text_only_first_query:
+        query = text_only_template.format(query)
+        text_only_first_query = False
     else:
-        image = Image.open(image_path).convert('RGB')
+        old_prompt = ''
+        for _, (old_query, response) in enumerate(history):
+            old_prompt += old_query + " " + response + "\n"
+        query = old_prompt + "USER: {} ASSISTANT:".format(query)
 
-    history = []
-
-    while True:
-        query = input("Human:")
-        if query == "clear":
-            break
-
-        if image is None:
-            if text_only_first_query:
-                query = text_only_template.format(query)
-                text_only_first_query = False
-            else:
-                old_prompt = ''
-                for _, (old_query, response) in enumerate(history):
-                    old_prompt += old_query + " " + response + "\n"
-                query = old_prompt + "USER: {} ASSISTANT:".format(query)
-        if image is None:
-            input_by_model = model.build_conversation_input_ids(
-                tokenizer,
-                query=query,
-                history=history,
-                template_version='chat'
-            )
-        else:
-            input_by_model = model.build_conversation_input_ids(
-                tokenizer,
-                query=query,
-                history=history,
-                images=[image],
-                template_version='chat'
-            )
-        inputs = {
-            'input_ids': input_by_model['input_ids'].unsqueeze(0).to(DEVICE),
-            'token_type_ids': input_by_model['token_type_ids'].unsqueeze(0).to(DEVICE),
-            'attention_mask': input_by_model['attention_mask'].unsqueeze(0).to(DEVICE),
-            'images': [[input_by_model['images'][0].to(DEVICE).to(TORCH_TYPE)]] if image is not None else None,
-        }
-        gen_kwargs = {
-            "max_new_tokens": 2048,
-            "pad_token_id": 128002,
-        }
-        with torch.no_grad():
-            outputs = model.generate(**inputs, **gen_kwargs)
-            outputs = outputs[:, inputs['input_ids'].shape[1]:]
-            response = tokenizer.decode(outputs[0])
-            response = response.split("<|end_of_text|>")[0]
-            print("\nCogVLM2:", response)
-        history.append((query, response))
+if image is None:
+    input_by_model = model.build_conversation_input_ids(
+        tokenizer,
+        query=query,
+        history=history,
+        template_version='chat'
+    )
+else:
+    input_by_model = model.build_conversation_input_ids(
+        tokenizer,
+        query=query,
+        history=history,
+        images=image_list,
+        template_version='chat'
+    )
+print(len(input_by_model['images']))
+inputs = {
+    'input_ids': input_by_model['input_ids'].unsqueeze(0).to(DEVICE),
+    'token_type_ids': input_by_model['token_type_ids'].unsqueeze(0).to(DEVICE),
+    'attention_mask': input_by_model['attention_mask'].unsqueeze(0).to(DEVICE),
+    'images': [[input_by_model['images'][0].to(DEVICE).to(TORCH_TYPE)]] if image is not None else None,
+}
+gen_kwargs = {
+    "max_new_tokens": 2048,
+    "pad_token_id": 128002,
+}
+with torch.no_grad():
+    outputs = model.generate(**inputs, **gen_kwargs)
+    outputs = outputs[:, inputs['input_ids'].shape[1]:]
+    response = tokenizer.decode(outputs[0])
+    response = response.split("<|end_of_text|>")[0]
+    print("\nCogVLM2:", response)
+history.append((query, response))
